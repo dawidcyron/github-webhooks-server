da0baf8c90966c7db0ee390a6ad9ae7d778881ec
Dawid Cyron <dawidcyron@op.pl>
2019-04-10 13:03:08 +0200

    Implement exception handling
    * Add support for use of environment variables
    * Add exception handling for missing headers and incorrect payload
    * Add Dockerfile to .gitignore

--- src/main/java/com/dawidcyron/githubwebhookshandler/GithubRepository/GithubRepositoryController.java
+++ src/main/java/com/dawidcyron/githubwebhookshandler/GithubRepository/GithubRepositoryController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.UnsatisfiedServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -19,31 +20,41 @@ public class GithubRepositoryController {
 
   Logger logger = LoggerFactory.getLogger(GithubRepositoryController.class);
   @Autowired GithubRepoRepository githubRepoRepository;
+
   @Value("${SECRET}")
   private String secret;
 
   @PostMapping("/payload")
   public void handleWebhook(
       @RequestBody String body,
-      @RequestHeader(value = "X-Hub-Signature", required = true) String signature) {
-    System.out.println(secret);
+      @RequestHeader(value = "X-Hub-Signature", required = true) String signature)
+      throws IOException {
     String digest = new HmacUtils(HmacAlgorithms.HMAC_SHA_256, secret).hmacHex(body);
     if (digest.equals(signature)) {
-      try {
-        ObjectMapper mapper = new ObjectMapper();
-        GithubRepositoryWrapper repositoryWrapper =
-            mapper.readValue(body, GithubRepositoryWrapper.class);
-        githubRepoRepository.save(repositoryWrapper.getRepository());
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+      ObjectMapper mapper = new ObjectMapper();
+      GithubRepositoryWrapper repositoryWrapper =
+          mapper.readValue(body, GithubRepositoryWrapper.class);
+      githubRepoRepository.save(repositoryWrapper.getRepository());
     }
-    System.out.println(digest);
   }
 
   @GetMapping("/")
   public ResponseEntity<List<GithubRepository>> getSixRecentRepositories() {
 
     return new ResponseEntity<>(githubRepoRepository.findTop6ByOrderById(), HttpStatus.OK);
+  }
+
+  @ExceptionHandler({IOException.class})
+  public ResponseEntity<String> handleIncorrectPayloadException() {
+    return new ResponseEntity<>(
+        "This server is supposed to be used only by Github Webhooks! Provided payload couldn't be unmarshalled!",
+        HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler({UnsatisfiedServletRequestParameterException.class})
+  public ResponseEntity<String> handleMissingRequestHeaderException() {
+    return new ResponseEntity<>(
+        "This server is supposed to be used only by Github Webhooks! Missing request header!",
+        HttpStatus.BAD_REQUEST);
   }
 }
